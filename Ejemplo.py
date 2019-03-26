4d7eb7db412a681aaa899ad4a3e58a411c61250d
alulab14 <alulab14@inf.pucp.edu.pe>
2019-03-26 15:40:29 -0500

    Tercer Intento

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,11 +1,11 @@
 for x in range(1,100):
-	if x%3==1 and x%5==1:
+	if x%3==0 and x%5==0:
 		print('FizzBuzz')
 	else:
-		if x%5==1:
+		if x%5==0:
 			print('Buzz')
 		else:
-			if x%3==1:
+			if x%3==0:
 				print('Fizz')
 			else:
 				print(x)
